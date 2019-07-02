94c165aa53bcb6bd75e0ecfd154ad168f4ee3035
diego gallardo <dhgallardo.fig@gmail.com>
2019-07-02 11:39:39 -0500

    bajar cambios de jc

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import React from 'react';
 //import logo from './logo.svg';
 import Menu from './components/menu/Menu';
 import Seccion1 from './components/seccion1/Seccion1';
-import Citas from './components/Citas/Citas';
 import Calculadora from './components/calculadora/Calculadora';
-import Testimonios from './components/testimonios/Testimonios';
-
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <Menu></Menu>      
       <Seccion1 ></Seccion1>
-      <Calculadora></Calculadora>
-      <Testimonios></Testimonios>
-      <Citas></Citas>
-     
+      <Calculadora></Calculadora>       
     </div>
   );
 }
