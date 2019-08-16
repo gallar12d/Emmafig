1fd1c0ea7d02381afad2aeb421c6c8dde763e856
diego gallardo <dhgallardo.fig@gmail.com>
2019-08-16 09:28:01 -0500

    falta la confirmacion y el loading de citas

--- src/components/citas/wizard/steps/Step4.js
+++ src/components/citas/wizard/steps/Step4.js
@@ -27,15 +27,27 @@ class Step4 extends Component {
         var elems = $('.mySelect');
 
         M.FormSelect.init(elems, {});
-        var elems = $('select');
+        elems = $('select');
 
         M.FormSelect.init(elems, {});
+
+        elems = $('#modal_cita')
+        M.Modal.init(elems, {});
+        $('#modal_cita').append('<button class="modal-close btn-flat" style="position:absolute;top:0;right:0;">x</button>');
 
         Axios.get(`https://emmafig.com/api1/profesionales`)
             .then(res => {
                 const profesionales = res.data;
                 this.setState({ profesionales });
             })
+
+    }
+    open_modal() {
+
+        var elem = $('#modal_cita')
+        var instance = M.Modal.getInstance(elem);
+        instance.open();
+
 
     }
 
@@ -127,9 +139,9 @@ class Step4 extends Component {
 
                         </div>
                         <div style={{ width: '100%' }} className='floting'>
-                            <div style={{ margin: '0px', color: 'black' }} className="input-field ">
-                                <select defaultValue={{ label: "Selecciona un turno disponible para esta fecha", value: 0 }}>
-                                    <option  >Completa tus datos básicos para la cita</option>
+                            <div onClick={this.open_modal.bind(this)} style={{ margin: '0px', color: 'black' }} className="input-field ">
+                                <select disabled value={'Completa tus datos básicos para la cita'} >
+                                    <option disabled  >Completa tus datos básicos para la cita</option>
 
                                 </select>
 
@@ -149,6 +161,81 @@ class Step4 extends Component {
                     <img alt="" src={this.state.imagen} />
 
                 </div>
+
+                <div id="modal_cita" className="modal">
+                    <div className="modal-content">
+                        <div className="row">
+                            <div className="col m12 left-align">
+                                <h5 style={{ fontWeight: 'bolder', fontFamily: 'lato', color: '#c83b8d' }}>
+                                    Datos necesarios para tu cita
+                              </h5>
+                            </div>
+
+                        </div>
+                        <div className="row">
+                            <div className="col m12">
+                            <hr></hr>
+
+                            </div>
+                           
+                        </div>
+
+                        <div className="row">
+
+                            <div className="input-field col s6 m6">
+                                <input placeholder="" id="primer_nombre" type="text" className="validate"></input>
+                                <label htmlFor="first_name">Primer nombre *</label>
+                            </div>
+                            <div className="input-field col s6 m6">
+                                <input placeholder="" id="segundo_nombre" type="text" className="validate"></input>
+                                <label htmlFor="first_name">Segundo nombre *</label>
+                            </div>
+                            <div className="input-field col s6 m6">
+                                <input placeholder="" id="primer_apellido" type="text" className="validate"></input>
+                                <label htmlFor="first_name">Primer apellido *</label>
+                            </div>
+                            <div className="input-field col s6 m6">
+                                <input placeholder="" id="segundo_apellido" type="text" className="validate"></input>
+                                <label htmlFor="first_name">Segundo apellido *</label>
+                            </div>
+                            <div className="input-field col s6 m6">
+                                <select>
+                                    <option disabled value="">Tipo de identificación</option>
+                                    <option value="1">Cédula de ciudadanía</option>
+                                    <option value="2">Tarjeta de identidad</option>
+                                    <option value="3">Cédula extrangera</option>
+                                </select>
+
+                            </div>
+                            <div className="input-field col s6 m6">
+                                <input placeholder="" id="numero_identificacion" type="text" className="validate"></input>
+                                <label htmlFor="first_name">Número de identificación *</label>
+                            </div>
+                            <div className="input-field col s6 m6">
+                                <input placeholder="" id="telefono1" type="text" className="validate"></input>
+                                <label htmlFor="first_name">Teléfono de contacto 1 *</label>
+                            </div>
+                            <div className="input-field col s6 m6">
+                                <input placeholder="" id="telefono2" type="text" className="validate"></input>
+                                <label htmlFor="first_name">Teléfono de contacto 2</label>
+                            </div>
+                            <div className="input-field col s12 m12">
+                                <input placeholder="" id="email" type="text" className="validate"></input>
+                                <label htmlFor="first_name">Correo electrónico</label>
+                            </div>
+
+
+
+                        </div>
+
+
+
+                    </div>
+                    <div className="modal-footer">
+                        <button  className="btn modal-close  ">Confirmar</button>
+                    </div>
+                </div>
+
 
             </div>
 
