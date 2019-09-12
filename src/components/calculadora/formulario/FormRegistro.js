967dccee8d04479ac8a5fde6bec65e33d3602f7c
Alejandro Anaya <jaanaya.fig@gmail.com>
2019-09-12 15:33:10 -0500

    abrir modal del login via url estable

--- src/components/calculadora/formulario/FormRegistro.js
+++ src/components/calculadora/formulario/FormRegistro.js
@@ -339,7 +339,7 @@ class FormRegistro extends Component {
                     <p className="center titulo-registro">Registrate</p>
                     <p className="center subtitulo-registro">Para recibir mas detalles de tu resultado <br />totalmente gratis</p>
                     <div id="btn_ingresar" className="row">
-                        <a className="waves-effect waves-light btn col s10 offset-s1" onClick={this.isLogin}>Ingresar</a>
+                        <a className="waves-effect waves-light btn col s10 offset-s1" id="btn_ingresar_a" onClick={this.isLogin}>Ingresar</a>
                     </div>
                     <div id="btn_registro" className="row">
                         <a className="waves-effect waves-light btn col s10 offset-s1" onClick={this.isRegistro}>Registrarse</a>
