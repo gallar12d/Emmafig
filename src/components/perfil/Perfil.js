2d040cc86e4bbd040f320d1f92b73a088f73e32a
Juan Carlos Vidal <jcvidal.fig@gamil.com>
2019-09-13 16:07:47 -0500

    cerrar sesion

--- src/components/perfil/Perfil.js
+++ src/components/perfil/Perfil.js
@@ -10,8 +10,13 @@ class Perfil extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            resultados: []
-
+            resultados: [],
+            primer_nombre: '',
+            segundo_nombre: '',
+            primer_apellido: '',
+            segundo_apellido: '',
+            fecha_nacimiento: '',
+            no_identificacion: ''
         }
     }
 
@@ -19,46 +24,68 @@ class Perfil extends Component {
     componentDidMount() {
         M.Tabs.init(this.Tabs);
         const userData = new FormData();
-        //asignar a no_identificacion el numero de cedula del usuario logeado
-        var no_identificacion = "1061699966";
-        userData.append("identificacion", no_identificacion)
-        //cambiar localhost
-        axios
-            .post("https://emmafig.com/api1/searchHistorialCitas", userData)
+        //axios.post('http://localhost/api1/rest-api-authentication-example/api/getIdentyById.php', {
+        axios.post('https://emmafig.com/api1/rest-authentication/api/getIdentyById.php', {
+            "id": localStorage.getItem('id')
+        }
+        )
             .then(res => {
-                let result = res.data;
+                this.setState({
+                    primer_nombre: res.data.primer_nombre,
+                    segundo_nombre: res.data.segundo_nombre,
+                    primer_apellido: res.data.primer_apellido,
+                    segundo_apellido: res.data.segundo_apellido,
+                    fecha_nacimiento: res.data.fecha_nacimiento,
+                    no_identificacion: res.data.no_identificacion
+                })
+                userData.append("identificacion", res.data.no_identificacion)
+                axios
+                    .post("https://emmafig.com/api1/searchHistorialCitas", userData)
+                    .then(res => {
+                        let result = res.data;
 
-                //actualizar estado 
-                //result = JSON.parse(result)
-                console.log(result.resultados_atl)
-                if(result.code == 200){
+                        //actualizar estado 
+                        //result = JSON.parse(result)
+                        console.log(result.resultados_atl)
+                        if (result.code == 200) {
 
-                    this.setState({ resultados: result.resultados_atl })
-                    tableData = result.resultados_atl.map(function (e) {
-    
-                        return <tr>
-                            <td>
-                                {e.fecha_atencion}
-                            </td>
-                            <td>
-                                {e.id_atencion}
-                            </td>
-                        </tr>
+                            this.setState({ resultados: result.resultados_atl })
+                            tableData = result.resultados_atl.map(function (e) {
+
+                                return <tr>
+                                    <td>
+                                        {e.fecha_atencion}
+                                    </td>
+                                    <td>
+                                        {e.id_atencion}
+                                    </td>
+                                </tr>
+                            })
+                        } else {
+                            document.getElementById("msj_error").innerHTML = "no se encontraron resultados"
+                        }
+                        if (tableData != null || result.code == 400) {
+
+                            var elem = document.getElementById("loader")
+                            elem.parentNode.removeChild(elem)
+                            return false
+
+
+                        }
+
+
                     })
-                }else{
-                    document.getElementById("msj_error").innerHTML = "no se encontraron resultados"
-                }
-                if(tableData != null || result.code == 400){
-                 
-                    var elem = document.getElementById("loader")
-                    elem.parentNode.removeChild(elem)
-                    return false
-                  
-
-                }
-
 
             })
+            .catch(function (error) {
+                if (error.response) {
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                }
+            });
+
+        //cambiar localhost
 
 
 
@@ -94,7 +121,7 @@ class Perfil extends Component {
                                 <div className="profile-img">
                                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt="" />
                                     <div className="file btn btn-lg btn-primary">
-                                        Change Photo
+                                        Cambiar Foto
                                 <input type="file" name="file" />
                                     </div>
                                 </div>
@@ -102,12 +129,12 @@ class Perfil extends Component {
                             <div className="col md6">
                                 <div className="profile-head">
                                     <h5>
-                                        Kshiti Ghelani
+                                        {this.state.primer_nombre+' '+this.state.segundo_nombre+' '+this.state.primer_apellido+' '+this.state.segundo_apellido}
                                     </h5>
                                     <h6>
-                                        N° identificacion: 1002846988
+                                        N° identificacion: {this.state.no_identificacion}
                                     </h6>
-                                    <p className="proile-rating">Fecha de nacimiento : <span>8/10</span></p>
+                                    <p className="proile-rating">Fecha de nacimiento : <span>{this.state.fecha_nacimiento}</span></p>
 
 
                                 </div>
@@ -131,20 +158,20 @@ class Perfil extends Component {
                                     <tbody>
                                         {tableData}
                                     </tbody>
-                                    
+
                                 </table>
-                                        <p id="msj_error"></p>
-                                        <div className="preloader-wrapper small active" id="loader">
-                                            <div className="spinner-layer spinner-green-only">
-                                                <div className="circle-clipper left">
-                                                    <div className="circle"></div>
-                                                </div><div className="gap-patch">
-                                                    <div className="circle"></div>
-                                                </div><div className="circle-clipper right">
-                                                    <div className="circle"></div>
-                                                </div>
-                                            </div>
+                                <p id="msj_error"></p>
+                                <div className="preloader-wrapper small active" id="loader">
+                                    <div className="spinner-layer spinner-green-only">
+                                        <div className="circle-clipper left">
+                                            <div className="circle"></div>
+                                        </div><div className="gap-patch">
+                                            <div className="circle"></div>
+                                        </div><div className="circle-clipper right">
+                                            <div className="circle"></div>
                                         </div>
+                                    </div>
+                                </div>
 
 
 
