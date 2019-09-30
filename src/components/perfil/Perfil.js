48bf2980671ea8fe2223eb462fc7430fa43a884a
Alejandro Anaya <jaanaya.fig@gmail.com>
2019-09-30 17:02:57 -0500

    filtro por resultados modulo de perfil y mostrar el tipo de identificacion

--- src/components/perfil/Perfil.js
+++ src/components/perfil/Perfil.js
@@ -4,6 +4,7 @@ import M from "materialize-css";
 import axios from "axios";
 import $ from 'jquery';
 
+
 let tableDataAtl;
 let tableDataEmf
 let Datagbl;
@@ -21,6 +22,7 @@ class Perfil extends Component {
             segundo_apellido: '',
             fecha_nacimiento: '',
             no_identificacion: '',
+            tipo_identificacion: '',
             file: ''
 
 
@@ -99,17 +101,17 @@ class Perfil extends Component {
         return capitalize;
 
     }
-    formatNumber (n) {
+    formatNumber(n) {
         n = String(n).replace(/\D/g, "");
-      return n === '' ? n : Number(n).toLocaleString();
+        return n === '' ? n : Number(n).toLocaleString();
     }
 
 
 
     componentDidMount() {
-        M.Tabs.init(this.Tabs);
-
-
+        M.AutoInit();
+        var loaderTabla = document.getElementById("loader")
+        loaderTabla.style.display = "inline-block"
 
         const userData = new FormData();
         //axios.post('http://localhost/api1/rest-api-authentication-example/api/getIdentyById.php', {
@@ -123,6 +125,37 @@ class Perfil extends Component {
                 var apellido1 = res.data.primer_apellido.toLowerCase();
                 var nombre2;
                 var apellido2;
+                var tipo_id = res.data.tipo_identificacion;
+                switch (tipo_id){
+                    case 'Cédula de ciudadanía':
+                                    this.setState({
+                                        tipo_identificacion: "C.C:"
+                                    })
+                        break;
+                    case 'Tarjeta de identidad':
+                        this.setState({
+                            tipo_identificacion: "T.I:"
+                        })
+                        break;
+                    case 'Registro civil':
+                        this.setState({
+                            tipo_identificacion: "R.C:"
+                        })
+                        break;
+                    case 'Cédula extrangera':
+                        this.setState({
+                            tipo_identificacion: "C.E:"
+                        })
+                        break;
+                    case 'NIT':
+                        this.setState({
+                            tipo_identificacion: "NIT:"
+                        })
+                        break;
+
+                }
+               
+                console.log(res.data);
                 if (res.data.segundo_nombre) {
 
                     nombre2 = res.data.segundo_nombre.toLowerCase();
@@ -143,7 +176,7 @@ class Perfil extends Component {
 
                 this.setState({
                     primer_nombre: this.capitalize(nombre1),
-                    segundo_nombre: this.capitalize(nombre2), 
+                    segundo_nombre: this.capitalize(nombre2),
 
                     primer_apellido: this.capitalize(apellido1),
                     segundo_apellido: this.capitalize(apellido2),
@@ -160,6 +193,7 @@ class Perfil extends Component {
                 //console.log(localStorage.getItem('id'))
                 var id_usuario = localStorage.getItem('id');
                 id_user.append('id_usuario', id_usuario);
+                id_user.append('filtro', 'todos');
                 axios
                     .post("https://emmafig.com/api1/searchProfilePicture", id_user)
                     .then(res => {
@@ -188,8 +222,11 @@ class Perfil extends Component {
 
 
 
-                    })
 
+
+
+                    })
+                    
 
                 axios
                     .post("https://emmafig.com/api1/searchHistorialCitas", id_user)
@@ -211,8 +248,12 @@ class Perfil extends Component {
                         }
                         if (this.state.resultados != null || result.code == 400 || this.state.resultados_emf != null) {
 
-                            var elem = document.getElementById("loader")
-                            elem.parentNode.removeChild(elem)
+                           
+                            loaderTabla.style.display = "none"
+
+
+
+
                             return false
 
 
@@ -254,6 +295,57 @@ class Perfil extends Component {
 
         })
 
+
+    }
+
+    serchResultadosFiltro(){
+        document.getElementById("msj_error").innerHTML = "";
+        var e = document.getElementById("selectResult")
+        this.setState({
+            resultados:[],
+            resultados_emf: []
+        })
+        var loaderTabla = document.getElementById("loader")
+        var tbody = document.getElementById("tbodyResult");
+        tbody.style.display = "none"
+        loaderTabla.style.display = "inline-block"
+        var selectValue = e.options[e.selectedIndex].value;
+        var dataForm = new FormData();
+        dataForm.append("id_usuario", localStorage.getItem("id"));
+        dataForm.append("filtro", selectValue);
+        axios
+        .post("https://emmafig.com/api1/searchHistorialCitas", dataForm)
+        .then(res => {
+            let result = res.data;
+
+            //actualizar estado 
+            //result = JSON.parse(result)
+
+
+
+            if (result.code == 200) {
+                this.setState({
+                    resultados: result.resultados_atl,
+                    resultados_emf: result.resultados_emf
+                })
+                
+            } else {
+                document.getElementById("msj_error").innerHTML = "no se encontraron resultados";
+            }
+            if (this.state.resultados != null || result.code == 400 || this.state.resultados_emf != null) {
+
+                loaderTabla.style.display = "none"
+                tbody.style.display = "inline-block"
+                
+
+
+
+
+                return false
+
+
+            }
+        })
 
     }
 
@@ -332,44 +424,6 @@ class Perfil extends Component {
         }
 
 
-
-
-
-        /*
-             let tableData = this.state.resultados.map(function (e) {
-                 strUrl = "https://fig.org.co/atlanticv2/pdf/" + e.abreviatura_servicio + "/" + e.id_atencion+"?emmafig=true";
-     
-                 return <tr>
-                     <td>
-                         {e.nombre_servicio}
-                     </td>
-                     <td>
-                         {e.fecha_atencion}
-                     </td>
-                     <td>
-                         <a target="_blank" href={strUrl}>Ver </a>
-                     </td>
-                 </tr>
-             })
- 
-         let tableDataEmf = this.state.resultados_emf.map(function(e){
-             strUrl = "https://fig.org.co/atlanticv2/pdf/" + e.abreviatura_servicio + "/" + e.id_atencion+"?emmafig=true";
-             return <tr>
- 
-             <td>
-                 ESTIMACION DE RIESGO
-             </td>
-             <td>
-                 {e.fecha_estimacion}
-             </td>
-             <td>
- 
-             </td>
-             </tr> 
-         })
-         */
-
-
         return (
 
             <div className="Perfil">
@@ -419,10 +473,10 @@ class Perfil extends Component {
 
 
                                 </h5>
+                                
                                 <h6>
-                                    N° identificacion: {this.state.no_identificacion}
+                                    {this.state.tipo_identificacion} {this.state.no_identificacion}
                                 </h6>
-                                <p className="proile-rating">Fecha de nacimiento : <span>{this.state.fecha_nacimiento}</span></p>
 
 
                             </div>
@@ -430,43 +484,78 @@ class Perfil extends Component {
 
                     </div>
                     <div className="row">
+                        
 
                         <div className="col l12 m12 s12">
-                            <h6>REGISTRO DE ACTIVIDAD</h6>
-                            <table className="striped" id="tableResult">
+                            <h3>HISTORIAL DE RESULTADOS</h3>
+                            <div className="filtroGroup">
+                            <div className="row rowFiltroGroup">
+
+                                <div className="col s12 m4 l4">
+                                    <h6 className="alig">¿Qué tipo de resultados deseas ver?</h6>
+                                </div>
+                                <div class="col s12 m4 l4">
+                                    <select id="selectResult">
+
+                                        <option value="todos">Todos</option>
+                                        <option value="atl">Resultados medicos</option>
+                                        <option value="emf">Estimacion de riesgo</option>
+
+                                    </select>
+
+                                </div>
+                                <div className="col s12 m4 l4 buttonContent">
+
+                                    <a class="waves-light btn-small" onClick={() => this.serchResultadosFiltro()}>Buscar</a>
+
+                                </div>
+                            </div>
+
+                        </div>
+                            <table className="striped" id="tableResultados" >
                                 <thead>
                                     <tr>
-                                        <th>Prueba</th>
-                                        <th>Fecha resultado</th>
                                         <th>Resultado</th>
+                                        <th>Fecha </th>
+                                        <th>Informe</th>
 
                                     </tr>
                                 </thead>
 
-                                <tbody>
+                                <p id="msj_error"></p>
+                                <div className="preloader-wrapper small active" id="loader">
+                                    <div className="spinner-layer spinner-green-only">
+                                        <div className="circle-clipper left">
+                                            <div className="circle"></div>
+                                        </div><div className="gap-patch">
+                                            <div className="circle"></div>
+                                        </div><div className="circle-clipper right">
+                                            <div className="circle"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <tbody id="tbodyResult">
+                                    
+
                                     {tableData}
                                     {tableDataEmf}
+                                   
+                                    
+                                    
+
                                 </tbody>
 
                             </table>
-                            <p id="msj_error"></p>
-                            <div className="preloader-wrapper small active" id="loader">
-                                <div className="spinner-layer spinner-green-only">
-                                    <div className="circle-clipper left">
-                                        <div className="circle"></div>
-                                    </div><div className="gap-patch">
-                                        <div className="circle"></div>
-                                    </div><div className="circle-clipper right">
-                                        <div className="circle"></div>
-                                    </div>
-                                </div>
-                            </div>
 
 
 
 
                         </div>
+
+
                     </div>
+
+
 
                 </div>
 
