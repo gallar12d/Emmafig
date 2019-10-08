12266574c73a8bbee2357a082811f81c524492be
jbvidal2006 <jbvidal2006@gmail.com>
2019-10-08 17:09:27 -0500

    cambios seccion 1, resonsive

--- src/components/seccion1/Seccion1.js
+++ src/components/seccion1/Seccion1.js
@@ -14,9 +14,9 @@ class CustomSlide extends Component {
     this.state = {};
   }
 
- 
 
- 
+
+
 
 
   render() {
@@ -26,25 +26,35 @@ class CustomSlide extends Component {
       <div  {...props}>
         <div id="div-seccion1" className="each-fade col s3 m6 l12" >
           <div className="image-container col s3 l3 offset-l2 offset-s1">
-        
-            <img className="panel" src={'https://emmafig.com/api1/public/images/sliders/' + imagen} />            
- 
+            <img className="panel" src={'https://emmafig.com/api1/public/images/sliders/' + imagen} />
+
             <img className="panelFondo" src={"img/blanco-nada" + alineacion + ".png"} />
             <div id="contenidoSlider " className={"info" + alineacion}>
               <div className="inner">
+                <div className="divTexto"><h2 className="textTitulo"><b>{titulo}</b></h2>
 
-                <div className="divTexto"><h3 className="textTitulo"><b>{titulo}</b><br /><br /></h3>
                   <h4>
                     <div className="content" dangerouslySetInnerHTML={{ __html: textoppal }}></div>
-
                   </h4>
                   <div id="PosicionButton">
-                    <a className="btn-small buttonSmall " href="http://blog.emmafig.com/">Conoce más</a>
+                    <a className="btn-small buttonSmall " href="http://blog.emmafig.com/" target="_blank">Conoce más</a>
                     <a className="btn-small buttonSmall" onClick={this.props.simulateClickFunction}>Calcula tu riesgo</a>
                     <div>
-                      <br /><br /><br /><br /><br />
+                      <br />
                     </div>
                   </div>
+                </div>
+                <div className="contenedorMovil">
+                <img id="avatar_emmaSeccion1" src={"img/imagenfondomovil.jpg"}></img>
+
+                  <div className="espacioVacio">
+                  <div className=""><h2 className="texto-encima"><b>{titulo}</b></h2></div>
+                  <div className="PosicionButtonMovil">
+                    <a className="btn-small buttonSmall " href="http://blog.emmafig.com/" target="_blank">Conoce más</a>
+                    <a className="btn-small buttonSmall" onClick={this.props.simulateClickFunction}>Calcula tu riesgo</a>
+                  </div>
+                    </div>
+                 
                 </div>
               </div>
             </div>
@@ -93,7 +103,7 @@ class Seccion1 extends Component {
 
   render() {
 
-    const listItems = this.state.sliders.map((d) => <CustomSlide simulateClickFunction = {this.props.activateScrollCalculadora} key={d.id_slider} id={d.id_slider} imagen={d.imagen} textoppal={d.texto_principal} textosec={d.texto_secundario} url_ver_mas={d.url_ver_mas} url_sec_boton={d.url_segundo_boton} titulo={d.nombre} alineacion={d.alineacion}></CustomSlide>);
+    const listItems = this.state.sliders.map((d) => <CustomSlide simulateClickFunction={this.props.activateScrollCalculadora} key={d.id_slider} id={d.id_slider} imagen={d.imagen} textoppal={d.texto_principal} textosec={d.texto_secundario} url_ver_mas={d.url_ver_mas} url_sec_boton={d.url_segundo_boton} titulo={d.nombre} alineacion={d.alineacion}></CustomSlide>);
 
     return (
       <section className="page-section layoutSeccion1" id="seccion1">
